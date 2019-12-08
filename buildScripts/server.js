5dd8eb56d2bc7939813251f92c0ffb8d1498baab
lesly <lkpsmx@gmail.com>
2019-12-07 23:36:21 -0600

    fix: merge

--- buildScripts/server.js
+++ buildScripts/server.js
@@ -18,7 +18,7 @@ app.use(require('webpack-dev-middleware')(compiler, {
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../src/views/index.html'));
+  res.sendFile(path.join(__dirname, '../src/views/home.html'));
 });
 
 app.listen(port, err => {
