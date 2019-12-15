326317e9666753bfbabebb80b43ab7f84edf4109
cout-lesly <57376779+lkps-mx@users.noreply.github.com>
2019-12-14 23:00:29 -0600

    Dev (#8)
    * chore: modify home page
    
    * chore: fix h2
    
    * chore: change home
    
    * add: home.js
    
    * add: home js

---
+++ src/js/home.js
@@ -0,0 +1,5 @@
+export default class Home{
+    dialog(){
+        return alert("home page");
+    }
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,11 @@
 import '../css/index.css';
 import User from '../js/user';
+import Home from '../js/home';
 
 let user = new User('nandi', 'nandi xd', 'tevaleverga@asdasd');
+let home = new Home();
+
+home.dialog();
 
 console.log('user=>', user.getHi()); //eslint-disable-line no-console
 console.log('user=>', user.email);//eslint-disable-line no-console
